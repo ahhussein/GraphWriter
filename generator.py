86a32680bd606544eadc5c98aa47650bb5bf39ed
Ahmed Hussein <a.abdelaziz91@gmail.com>
2020-10-03 18:11:36 +0200

    combine generator and evaulator

--- generator.py
+++ generator.py
@@ -5,6 +5,20 @@ from lastDataset import dataset
 from models.newmodel import model 
 from pargs import pargs,dynArgs
 #import utils.eval as evalMetrics
+import glob
+import logging
+from eval import Evaluate
+
+logger = logging.getLogger('myapp')
+hdlr = logging.FileHandler('eval-graph.log')
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.DEBUG)
+
+
+
+evaluator = Evaluate()
 
 def tgtreverse(tgts,entlist,order):
   entlist = entlist[0]
@@ -20,12 +34,13 @@ def tgtreverse(tgts,entlist,order):
 def test(args,ds,m,epoch='cmdline'):
   args.vbsz = 1
   model = args.save.split("/")[-1]
-  ofn = "outputs/"+model+".beam_predictions"
   m.eval()
   k = 0
   data = ds.mktestset(args)
   ofn = "outputs/"+model+".inputs.beam_predictions."+epoch
+  ofng = "outputs/"+model+".inputs.beam_gt."+epoch
   pf = open(ofn,'w')
+  pfg = open(ofng,'w')
   preds = []
   golds = []
   for b in data:
@@ -42,14 +57,28 @@ def test(args,ds,m,epoch='cmdline'):
     gen = ds.reverse(gen.done[0].words,b.rawent)
     k+=1
     gold = ds.reverse(b.tgt[0][1:],b.rawent)
-    print(gold)
-    print(gen)
-    print()
     preds.append(gen.lower())
     golds.append(gold.lower())
     #tf.write(ent+'\n')
     pf.write(gen.lower()+'\n')
-  m.train()
+    pfg.write(gold.lower() + '\n')
+  pf.close()
+  pfg.close()
+
+  with open(ofn) as f:
+    cands = {'generated_description' + str(i): x.strip() for i, x in enumerate(f.readlines())}
+  with open(ofng) as f:
+    refs = {'generated_description' + str(i): [x.strip()] for i, x in enumerate(f.readlines())}
+
+  final_scores = evaluator.evaluate(live=True, cand=cands, ref=refs)
+  logger.info("Results for model:\t", model_name)
+  logger.info ('Bleu_1:\t', final_scores['Bleu_1'])
+  logger.info ('Bleu_2:\t', final_scores['Bleu_2'])
+  logger.info ('Bleu_3:\t', final_scores['Bleu_3'])
+  logger.info ('Bleu_4:\t', final_scores['Bleu_4'])
+  logger.info ('ROUGE_L:\t', final_scores['ROUGE_L'])
+  logger.info ('METEOR:\t', final_scores['METEOR'])
+
   return preds,golds
 
 '''
@@ -67,16 +96,19 @@ if __name__=="__main__":
   ds = dataset(args)
   args = dynArgs(args,ds)
   m = model(args)
-  cpt = torch.load(args.save)
-  m.load_state_dict(cpt)
   m = m.to(args.device)
+  models = glob.glob(args.save+"/*vloss*")
   m.args = args
   m.maxlen = args.max
   m.starttok = ds.OUTP.vocab.stoi['<start>']
   m.endtok = ds.OUTP.vocab.stoi['<eos>']
   m.eostok = ds.OUTP.vocab.stoi['.']
   args.vbsz = 1
-  preds,gold = test(args,ds,m)
+
+  for i, model_name in enumerate(models):
+    cpt = torch.load(model_name)
+    m.load_state_dict(cpt)
+    preds,gold = test(args,ds,m)
   '''
   scores = metrics(preds,gold)
   for k,v in scores.items():
